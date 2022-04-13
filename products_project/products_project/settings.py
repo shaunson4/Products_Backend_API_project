611a7fdf0eaf50c9b31cd7f7e6bff9d8e2ec2aa5
shaunson4 <sgray4106@gmail.com>
2022-04-13 14:33:07 -0500

    Created Local Settings file, removed settings info over into local settings file + Revised Databases info to reflect accurate info for Products project as outlined

--- products_project/products_project/settings.py
+++ products_project/products_project/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for products_project project.
 
@@ -19,8 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-b)$)=@06)f-zl=b=bc1sguh9iksrnjiu6h7vwy96@k#u*(w+-y'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -73,12 +73,6 @@ WSGI_APPLICATION = 'products_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 
 # Password validation
@@ -121,3 +115,8 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+try:
+    from products_project.local_settings import *
+except ImportError:
+    pass
